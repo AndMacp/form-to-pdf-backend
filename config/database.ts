e454843924b6c56b2bb893aec65112f4fa285a42
Andrejs Macipura <andrejs123@gmail.com>
2025-03-21 13:03:49 +0200

    Add mysql2 dependency for production

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,30 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: env('DATABASE_URL'),
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-  },
-})
+import path from 'path'
+
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'mysql')
+
+  const connections = {
+    mysql: {
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+    },
+  }
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: 60000,
+    },
+  }
+}
